505ba778701896364ba969c576d4c0ea6b185076
krenar.berisha <krenar.berisha@bevuta.com>
2019-08-20 15:29:54 +0200

    Add button to change the colors

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 
-var pickedColor =colors[4];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click" ,function(){
+
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent=pickedColor;
+
+	for(var i = 0; i<squares.length; i++){
+		squares[i].style.background =colors[i];
+	}
+	h1.style.backgroundColor="#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,11 +31,44 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 
 		if (clickedColor === pickedColor) {
-			alert("correct");
+			messageDisplay.textContent = "Correct !";
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+			resetButton.textContent="Play again !";
+
 		}
 			else{
 				this.style.background ="#232323";
-			}
-		
+				messageDisplay.textContent ="try again";
+			}		
 	});
-}   
+}     
+
+function changeColors(color){
+
+	for (var i = 0; i<squares.length; i++){
+
+		squares[i].style.backgroundColor=color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+
+}
+function generateRandomColors(num){
+
+	var arr = [];
+	for(var i = 0; i < num; i++){ 
+		arr.push(randomColor())
+
+	}
+	return arr;
+}
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r +", " + g + ", " + b+")";
+}
